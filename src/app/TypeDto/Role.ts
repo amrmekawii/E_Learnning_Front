eecac1c80b969126f3b3ed77da45a894664daf3a
abdelrahman Hamdy <abdoe317111@gmail.com>
2023-09-11 07:45:15 +0300

    .

---
+++ src/app/TypeDto/Role.ts
@@ -0,0 +1,5 @@
+export enum Role{
+
+
+    Sadmin, Student, Admin
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { IntroComponent } from './Components/intro/intro.component';
 import { LoginComponent } from './Components/login/login.component';
 import { RegisterComponent } from './Components/register/register.component';
 import { HomeComponent } from './Components/Admin/Home/home/home.component';
+import { StudentMangmentComponent } from './Components/Admin/UserMangment/student-mangment/student-mangment.component';
 
 const routes: Routes = [
 
@@ -11,7 +12,7 @@ const routes: Routes = [
   { path: "", component: IntroComponent },
   { path: "Register", component: RegisterComponent },
   { path: "Login", component: LoginComponent },
-  {path: "AdminHome" , component:HomeComponent}
+  {path: "AdminHome" , component:HomeComponent, children:[{path:"StudentMangment", component: StudentMangmentComponent}]}
 ];
 
 @NgModule({

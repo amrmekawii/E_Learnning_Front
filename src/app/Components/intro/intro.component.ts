b4dc6fc96b9b31a19ecbbec1107b6073a1b3f7c0
amrmekawii <65632088+amrmekawii@users.noreply.github.com>
2023-09-28 19:08:29 +0300

    amrss

--- src/app/Components/intro/intro.component.ts
+++ src/app/Components/intro/intro.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as Aos from 'aos';
 import { HostListener } from '@angular/core';
+import { AuthenticationServiceService } from 'src/app/Services/UserAuthentication/authentication-service.service';
 
 
 
@@ -13,10 +14,19 @@ import { HostListener } from '@angular/core';
 
 export class IntroComponent implements OnInit {
 
-
-
+  constructor(private auth: AuthenticationServiceService) { }
+  Islogin!: boolean
+  role?: any
+  
   ngOnInit(): void {
     Aos.init();
+    this.Islogin = this.auth.isLoggedIn$.value
+    this.role = this.auth.UserData.role
+  
+  }
+  isScrolled = false;
 
-
-  }}
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    this.isScrolled = window.scrollY > 100; }// Adjust the scroll threshold
+}
